9e21030679166c09c6c43af4dfc35d2d5b08b01c
TuriniMattia <turinimattia96@gmail.com>
2024-06-06 16:45:51 +0200

    [add]creazione di una home veloce

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<div class="container ">
-    progetto portfolio
+<div class="container mt-5">
+    <h1>Portafolio personale Mattia Turini</h1>
+    <p>In questo portafolio troverete tutti i progetti migliori o più interessanti che ho reallizato durante il percorso Boolean. Per il momento questa è una versione base,o se vogliamo entrare nel linguaggio del settore , un Alpha (o una pre-Alpha ). Verrà aggiornata constantemente implementando tutto quello che abbiamo visto, diventanto cosi, essa stessa un progetto da mostrare. </p>
+</div>
+<div class="container mt-5">
+    <button class="btn btn-success">Vai ai progetti</button>
 </div>
 
 
