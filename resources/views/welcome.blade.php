9b00ae2c563fc8047d223cf213c1ce7178768b1f
TuriniMattia <turinimattia96@gmail.com>
2024-06-06 16:54:58 +0200

    [add]aggiunta rotta index al button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <p>In questo portafolio troverete tutti i progetti migliori o più interessanti che ho reallizato durante il percorso Boolean. Per il momento questa è una versione base,o se vogliamo entrare nel linguaggio del settore , un Alpha (o una pre-Alpha ). Verrà aggiornata constantemente implementando tutto quello che abbiamo visto, diventanto cosi, essa stessa un progetto da mostrare. </p>
 </div>
 <div class="container mt-5">
-    <button class="btn btn-success">Vai ai progetti</button>
+    <a class="btn btn-success" href="{{route('boolfolios.index')}}">Vai ai progetti</a>
 </div>
 
 
